3507b46c3c19efdb30d183ef707b5b40199ff39a
BELGHAR <ayoub.belghar@capgemini.com>
2018-03-12 17:35:24 +0000

    todolist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { TodolistComponent } from './todolist/todolist.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodoService } from './todo.service';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodolistComponent,
+    TodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
